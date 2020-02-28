71ea71fb436f4e9728b282231ca94fe57fe88d21
liyanchen <329257132@qq.com>
2020-02-29 07:06:20 +0800

    'login分支'

--- assets_li/js/li_login.js
+++ assets_li/js/li_login.js
@@ -1,50 +1,58 @@
 //入口函数
-$(function () {
-    var layer = layui.layer;
-    var form = layui.form;
-    //先设置link_reg点击事件
-    $(".link_reg").on("click", function () {
-        $(".login_box").hide();
-        $(".reg_box").show();
-    });
-    //先设置link_login点击事件
-    $(".link_login").on("click", function () {
-        $(".login_box").show();
-        $(".reg_box").hide();
-    });
-    //自定义校验规则
-    form.verify({
-        pass: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-        somepwd: function (value) {
-            var pass = $(".reg_box [name=password]").val();
-            if (value !== pass) {
-                return "两次密码不一致"
-            }
+$(function() {
+  var layer = layui.layer;
+  var form = layui.form;
+  //先设置link_reg点击事件
+  $(".link_reg").on("click", function() {
+    $(".login_box").hide();
+    $(".reg_box").show();
+  });
+  //先设置link_login点击事件
+  $(".link_login").on("click", function() {
+    $(".login_box").show();
+    $(".reg_box").hide();
+  });
+  //自定义校验规则
+  form.verify({
+    pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
+    somepwd: function(value) {
+      var pwd = $(".reg_box [name=password]").val();
+      if (value !== pwd) {
+        return "两次密码不一致";
+      }
+    }
+  });
+  //注册页面
+  $("#form-reg").on("submit", function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "http://www.liulongbin.top:3007/api/reguser",
+      data: $(this).serialize(),
+      success: function(res) {
+        if (res.status !== 0) {
+          return layer.msg("注册失败");
         }
-
+        layer.msg("注册成功,正在登陆");
+        $(".link_login").click();
+      }
     });
-    //设置注册事件
-    $(".lg_gin").on("submit", function (e) {
-        //阻止表单默认提交
-        e.preventDefault();
-        //发送注册请求
-        $.ajax({
-            type: "POST",
-            url: "http://www.liulongbin.top:3007/api/reguser",
-            data: $(this).serialize(),
-            success: function (res) {
-                if (res.status !== 0) {
-                    return layer.msg('res.message');
-                }
-                layer.msg('注册成功,请登录');
-                //触发登录框跳转
-                $(".link_login").click();
-            }
-        });
-        //设置提交事件
-
-
-
-
-    })
-})
+  });
+  //登录页面
+  $("#form-login").on("submit", function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "http://www.liulongbin.top:3007/api/login",
+      data: $(this).serialize(),
+      success: function(res) {
+        if (res.status !== 0) {
+          return layer.msg(res.message);
+        }
+        layer.msg("登陆成功");
+        localStorage.setItem("token", res.token);
+        location.href = "/li_index.html";
+      }
+    });
+  });
+});
